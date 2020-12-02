abc4767e96dc078b2861eb4568b7cf715936b2d5
pomkalk <pomkalk@gmail.com>
2020-12-02 16:19:46 +0700

    t1

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const env = require('./lib/env')
 const { createLogger } = require('./lib/logger')
 const Router = require('koa-router')
 const Static = require('koa-static')
+const fs = require('fs')
 
 const app = new Koa()
 const adminRouter = new Router()
@@ -13,11 +14,10 @@ console.log(env)
 app.use(Static(env.admindir))
 
 
-// adminRouter.get('/admin:p(.*)', (ctx) => {
-//     ctx.body = `<pre>${JSON.stringify(ctx.params, null, 2)}</pre>`
-//     console.log(ctx.params)
-// })
+adminRouter.get('/admin:p(.*)', (ctx) => {
+    ctx.body = fs.readFileSync(env.admindir+'/admin/index.html').toString()
+})
 
-// app.use(adminRouter.routes()).use(adminRouter.allowedMethods())
+app.use(adminRouter.routes()).use(adminRouter.allowedMethods())
 
 app.listen(3000)
