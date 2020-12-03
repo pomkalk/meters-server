5c66ce675ea1e32393a659ef1ab7e88237c14f6e
pomkalk <pomkalk@gmail.com>
2020-12-03 16:53:10 +0700

    login api

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
-const Koa = require('koa')
-const postgres = require('postgres')
-const env = require('./lib/env')
 const { createLogger } = require('./lib/logger')
-const Router = require('koa-router')
-const Static = require('koa-static')
-const fs = require('fs')
+const App = require('./lib/app')
+const { sql, sqlCheck } = require('./lib/db')
 
-const app = new Koa()
-const adminRouter = new Router()
+const logger = createLogger('server')
 
-app.use(Static(env.admindir))
+const main = async () => {
 
+    if (await sqlCheck) {
+        logger.info('Database connected')
+        const app = new App(sql)
+        app.start()
+    } else {
+        logger.error('Database error')
+    }
+}
 
-adminRouter.get('/admin:p(.*)', (ctx) => {
-    ctx.body = fs.readFileSync(env.admindir+'/admin/index.html').toString()
-})
-
-app.use(adminRouter.routes()).use(adminRouter.allowedMethods())
-
-app.listen(3000)
+main()
