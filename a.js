99736dee0d92cfb0883698341abaa0b2c67d0966
pomkalk <pomkalk@gmail.com>
2021-02-11 09:20:58 +0700

    SEND NOTIFICATION

--- a.js
+++ a.js
@@ -1,26 +1,50 @@
-// const env = require('./lib/env')
-// const postgres = require('postgres')
-// let sql = postgres(env.db)
+const env = require('./lib/env')
+const postgres = require('postgres')
+let sql = postgres(env.db)
+let faker = require('faker')
 
-
-// const main = async () => {
-//     let a = await sql`SELECT '["asd","dsa", 123]'::jsonb ? '123'`
-//     //let a = await sql`SELECT id, target->'list' as list FROM a WHERE (target->>'list')::jsonb ? '103030'`
-//     console.log(JSON.stringify(a, null, 2))
-    
-
-// }
-
-// main()
-
-
-let a = {
-    data: [1, 2, 3],
-    name: 'ogogo'
+const addNick = async (name) => {
+    await sql.begin(async sql=>{
+        let [x] = await sql`insert into nicks (name) values (${name}) returning *`
+        let data = {
+            ref: x.id,
+            list: [
+                {
+                    value: x.name,
+                    date: x.created_at
+                }
+            ]
+        }
+        await sql`insert into nicks_hist (data) values (${ sql.json(data) })`
+    })
 }
 
-const f = ({data: res, name: name}) => {
-    console.log(res, name)
+const updateNick = async (id, name) => {
+    await sql.begin(async sql => {
+        let [x] = await sql`UPDATE nicks SET name=${name}, updated_at=now() WHERE id=${id} RETURNING *`
+        let z = [{
+            value: x.name,
+            date: x.updated_at
+        }]
+        await sql`UPDATE nicks_hist SET data = jsonb_set(data, '{list}', data->'list'|| ${sql.json(z)}) WHERE data->'ref' = ${ sql.json(x.id)}`
+    })
 }
 
-f(a)
+const main = async () => {
+    try {
+        let q = Array.from({length: 1000}, ()=>faker.name.findName())
+        for (let x of q) {
+            let id = faker.random.number({min: 1, max: 3})
+            await updateNick(id, x)
+        }
+        
+    } catch (e) {
+        console.log(e)
+    } finally {
+        await sql.end()
+    }
+}
+
+main()
+
+
