a9898f1ba7a612b1b48a7665f1075cc4daaffb42
pomkalk <pomkalk@gmail.com>
2021-02-15 13:38:40 +0700

    suppliers

--- a.js
+++ a.js
@@ -1,50 +1,10 @@
-const env = require('./lib/env')
-const postgres = require('postgres')
-let sql = postgres(env.db)
-let faker = require('faker')
+const parse = require('parsedbf')
+const fs = require('fs')
 
-const addNick = async (name) => {
-    await sql.begin(async sql=>{
-        let [x] = await sql`insert into nicks (name) values (${name}) returning *`
-        let data = {
-            ref: x.id,
-            list: [
-                {
-                    value: x.name,
-                    date: x.created_at
-                }
-            ]
-        }
-        await sql`insert into nicks_hist (data) values (${ sql.json(data) })`
-    })
-}
+let a = parse(fs.readFileSync("./import-data/2020/8/S_ADDR.DBF"), "cp866")
 
-const updateNick = async (id, name) => {
-    await sql.begin(async sql => {
-        let [x] = await sql`UPDATE nicks SET name=${name}, updated_at=now() WHERE id=${id} RETURNING *`
-        let z = [{
-            value: x.name,
-            date: x.updated_at
-        }]
-        await sql`UPDATE nicks_hist SET data = jsonb_set(data, '{list}', data->'list'|| ${sql.json(z)}) WHERE data->'ref' = ${ sql.json(x.id)}`
-    })
-}
+let s = new Set()
 
-const main = async () => {
-    try {
-        let q = Array.from({length: 1000}, ()=>faker.name.findName())
-        for (let x of q) {
-            let id = faker.random.number({min: 1, max: 3})
-            await updateNick(id, x)
-        }
-        
-    } catch (e) {
-        console.log(e)
-    } finally {
-        await sql.end()
-    }
-}
+a.forEach(x=>s.add(x.ORG))
 
-main()
-
-
+console.log(s)
